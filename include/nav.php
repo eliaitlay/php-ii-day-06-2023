6681ea5af88ebec9810d326adff64effb9860f78
Elia itlay <itlayelia@gmail.com>
2023-04-11 20:18:41 +0900

    finish day06

---
+++ include/nav.php
@@ -0,0 +1,9 @@
+<nav class="border border-gray-600 container mx-auto p-6">
+        <a href="?page=beranda" class="font- border py-3 px-4 bg-gray-700 hover:bg-gray-600 transition text-gray-100">Beranda</a> 
+        <a href="?category=teknologi" class="font- border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Teknologi</a>
+        <a href="?category=pendidikan" class="font- border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Pendidikan</a>
+        <a href="?category=pengembangan-diri" class="font- border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Pengembangan Diri</a>
+        <a href="?category=kursus" class="font- border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Kursus</a>
+        <a href="?kategory=komunitas" class="font- border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Komunitas</a>
+        <a href="?kategory=lainnya" class="font- border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition">Lainnya</a>
+    </nav>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    
+    <!-- HEADER START -->
+
+    <?php include('include/header.php'); ?>
+
+    <!-- HEADER END -->
+    <!-- NAV START -->
+
+    <?php include('include/nav.php'); ?>
+
+    <!-- NAV END -->
+    <!-- MAIN CONTENT START -->
+
+    <?php 
+        if(isset($_GET['page']) && $_GET['page'] == 'beranda'){
+            include('pages/beranda.php');
+        }else {
+            include('pages/kategori.php');
+        }
+    ?>
+
+    <!-- MAIN CONTENT END -->
+    <!-- FOOTER START -->
+
+    <?php include('include/footer.php'); ?>
+
+    <!-- FOOTER END -->
+</body>
+</html>
